faa9c7c81f6a0cccd18e8c5e4e3f064768228412
Vinicius Trindade <contato@viniciustrindade.com.br>
2016-03-26 20:07:51 -0300

    Grafico RealTime

--- client/app.py
+++ client/app.py
@@ -20,15 +20,21 @@ sensor_type_list = [\
 
 sensor_list = [99996,99997,99998,99999]
 
-defaultJson={ "chart" : {  "type": "line", "height": 350},\
-"plotOptions": {"series": { "animation": "false"}},\
-"series":[{"name": 'Label1', "data": [1,2,3,8]}, {"name": 'Label2', "data": [4, 5, 6,9]}],\
-"title":{"text": 'Temperatura (SR04#99999)'},\
-"xAxis":{"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']},\
-"yAxis":{"title": {"text": 'yAxis Label'}}}
+# defaultJson={ "chart" : {  "type": "line", "height": 350},\
+# "plotOptions": {"series": { "animation": "false"}},\
+# "series":[{"name": 'Label1', "data": [1,2,3,8]}, {"name": 'Label2', "data": [4, 5, 6,9]}],\
+# "title":{"text": 'Temperatura (SR04#99999)'},\
+# "xAxis":{"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']},\
+# "yAxis":{"title": {"text": 'yAxis Label'}}}
 
 #Dados gerados pelos testes de Ailton
-defaultJson={"chart": {"height": 350, "type": "line"}, "legend": {"legend": {"align": "right", "borderWidth": 0, "layout": "vertical", "verticalAlign": "middle"}}, "series": [{"data": [6.75, 6.17, 7.89, 9.92, 9.74, 8.57, 9.28, 8.71, 6.12, 8.47], "name": "HC-SR04"}], "subtitle": {"text": "Medindo distancia", "x": -20}, "title": {"text": "Sensor HC-SR04", "x": -20}, "tooltip": {"tooltip": {"valueSuffix": "cm"}}, "xAxis": {"categories": ["18:18:53", "18:18:53", "18:18:53", "18:18:53", "18:18:53", "18:18:53", "18:18:53", "18:18:53", "18:18:53", "18:18:53"]}, "yAxis": {"plotLines": [{"color": "#808080", "value": 0, "width": 1}], "title": {"text": "Distancia (cm)"}}}
+defaultJson={"chart": {"height": 350, "type": "line"}, 
+"legend": {"legend": {"align": "right", "borderWidth": 0, "layout": "vertical", "verticalAlign": "middle"}}, 
+"series": [{"data": [6.75, 6.17, 7.89, 9.92, 9.74, 8.57, 9.28, 8.71, 6.12, 8.47], "name": "HC-SR04"}], 
+"subtitle": {"text": "Medindo distancia", "x": -20}, "title": {"text": "Sensor HC-SR04", "x": -20}, 
+"tooltip": {"tooltip": {"valueSuffix": "cm"}}, 
+"xAxis": {"categories": ["18:18:53", "18:18:53", "18:18:53", "18:18:53", "18:18:53", "18:18:53", "18:18:53", "18:18:53", "18:18:53", "18:18:53"]}, 
+"yAxis": {"plotLines": [{"color": "#808080", "value": 0, "width": 1}], "title": {"text": "Distancia (cm)"}}}
 
 @app.route('/')
 @app.route('/index')
@@ -44,11 +50,15 @@ def getTipos():
 def startSensor():
   tipo = request.args.get('tipo')
   portas = request.args.get('portas')
+  #salvar no banco e retornar o id
+  #inicia uma thread lendo o sensor de Junior
+  #executar(tipo,portas); -> guardar na variavel global para futura recuperacao. #id, thread
   return json.dumps(99999)
 
 @app.route('/api/sensor/stop')
 def stopSensor():
   sensor_id = request.args.get('sensor_id')
+  #procurarETerminar(sensor_id)
   return json.dumps("STOPPED")
 
 
@@ -60,12 +70,13 @@ def listAll():
 def getChart():
   sensor_id = request.args.get('sensor_id')
   defaultJson["series"][0]["data"].append(random.randint(0,99));
-  defaultJson["series"][1]["data"].append(random.randint(0,99));
+ # defaultJson["series"][1]["data"].append(random.randint(0,99));
   defaultJson["xAxis"]["categories"].append(strftime("%H:%M:%S",gmtime()))
+  
   if(len(defaultJson["series"][0]["data"]) > 10):
       defaultJson["series"][0]["data"].pop(0)
-  if(len(defaultJson["series"][1]["data"]) > 10):
-      defaultJson["series"][1]["data"].pop(0)
+  #if(len(defaultJson["series"][1]["data"]) > 10):
+   #   defaultJson["series"][1]["data"].pop(0)
   if(len(defaultJson["xAxis"]["categories"]) > 10):
       defaultJson["xAxis"]["categories"].pop(0)
   return json.dumps(defaultJson)
