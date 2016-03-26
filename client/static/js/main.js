4f6017f7967305b1643a13560414dd3988f36772
Vinicius Trindade <contato@viniciustrindade.com.br>
2016-03-26 11:28:21 -0300

    Criando Cliente

--- client/static/js/main.js
+++ client/static/js/main.js
@@ -7,3 +7,49 @@ $(document).ready(function() {
     series: series
   });
 });
+
+
+angular.module('raspboardpyApp', [])
+.controller('dashboardController', ['$scope','$http','$interval', function($scope,$http,$interval) {
+  
+    $interval(function(){
+      $http.get('http://localhost:8080/instances').
+      success(function(data) {
+          $scope.instances = data;
+      });
+   
+    },1000);
+
+    $scope.addSensor = function(){
+      $http.get('http://localhost:8080/instances/new').
+        success(function(data) {
+          if (data =="OK"){
+            $scope.hassuccess = "Executada com Sucesso."
+          }else{
+            $scope.haserror = data;
+          }
+        });
+    }
+    $scope.execute = function(){
+      $http.post('http://localhost:8080/execute?sql=' + $scope.query).
+        success(function(data) {
+          console.log(data);
+          if (data =="OK"){
+            $scope.hassuccess = "Executada com Sucesso."
+          }else{
+            $scope.haserror = data;
+          }
+        });
+    }
+     $scope.stop = function(id){
+        $http.post('http://localhost:8080/instances/stop/?id=' + id).
+        success(function(data) {
+          if (data =="OK"){
+                $scope.hassuccess = "Executada com Sucesso."
+            }else{
+            $scope.haserror = data;
+            }
+        });
+    }
+   
+}]);
